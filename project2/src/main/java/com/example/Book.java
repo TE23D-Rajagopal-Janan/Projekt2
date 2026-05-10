7872097631bf5fa1f313563c7daa834f9ddb7454
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2026-05-10 20:56:15 +0200

    wdad

---
+++ project2/src/main/java/com/example/Book.java
@@ -0,0 +1,36 @@
+package com.example;
+
+public class Book extends LibraryItem {
+    
+    private String author;
+    private String genre;
+    private int pages;
+
+    public Book(String id, String title, boolean isAvailable,
+                String author, String genre, int pages) {
+
+        super(id, title, isAvailable);
+
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    @Override
+    public String getInfo() {
+        return getTitle() + " - " + author;
+    }
+    
+}
---
+++ project2/src/main/java/com/example/LibraryItem.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public abstract class LibraryItem {
+    
+    private String id;
+    private String title;
+    private boolean isAvailable;
+
+    public LibraryItem(String id, String title, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean status) {
+        isAvailable = status;
+    }
+
+    public abstract String getInfo();
+    
+}
