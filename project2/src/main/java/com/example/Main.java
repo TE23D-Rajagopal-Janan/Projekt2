d7dd30cfeeb550e3d7ffc6c3ebb8bbeb5d52284e
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2026-05-14 19:56:34 +0200

    inte klar hätma ifrån server

--- project2/src/main/java/com/example/Main.java
+++ project2/src/main/java/com/example/Main.java
@@ -2,8 +2,17 @@
 // Main klass med meny och ArrayList
 
 package com.example;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import kong.unirest.core.Unirest;
+import kong.unirest.core.HttpResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,27 +21,52 @@ public class Main {
 
     ArrayList<Book> books = new ArrayList<>();
     ArrayList<Magazine> magazines = new ArrayList<>();
-        
+
     boolean meny = true;
 
     while (meny) {
-    System.out.println("Meny");
-    System.out.println("1. Lägg till en bok");
-    System.out.println("2. Lägg till en tidning");
-    System.out.println("3. Visa alla böcker");
-    System.out.println("4. Visa alla tidningar");
-    System.out.println("5. Avsluta");
-    }
+        System.out.println("--- MENY ---");
+        System.out.println("1. Hämta böcker");
+        System.out.println("2. Lägg till bok");
+        System.out.println("3. Visa böcker");
+        System.out.println("4. Avsluta");
 
-    int choice = scanner.nextInt();
-    scanner.nextLine();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
 
-    if (choice == 1) {
+         // HÄMTA BÖCKER
+        if (choice == 1) {
+            HttpResponse<String> response = Unirest.get("http://10.151.168.5:3111/books").asString();
+            
+            String json = response.getBody();
+            Gson gson = new Gson();
+            
+            Type bookListType =
+            new TypeToken<ArrayList<Book>>(){}.getType();
+            books = gson.fromJson(json, bookListType);
 
-        System.out.println("Skriv in följande");
-        System.out.println("Title: ");
+            System.out.println("alla böcker hämtade");
+            }
+    
+    else if (choice == 2) {
+        System.out.print("Titel: ");
         String title = scanner.nextLine();
-    }
 
+        System.out.print("Kategori: ");
+        String category = scanner.nextLine();
+
+        System.out.print("Issue Number: ");
+        int issue = scanner.nextInt();
+
+        System.out.print("År: ");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        
+        Magazine newMagazine = new Magazine("1",title,true,issue,category, year);
+        
+        magazines.add(newMagazine);
+        System.out.println("Tidning tillagd");
     }
+}
+}
 }
